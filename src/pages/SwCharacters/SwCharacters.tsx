f7df2188096a43459ee7bd01ff19f1d21100c763
Mateusz Kałucki <mateusz.kalucki@pl.abb.com>
2022-06-13 15:18:31 +0200

    element instead of function, useState value fix

--- src/pages/SwCharacters/SwCharacters.tsx
+++ src/pages/SwCharacters/SwCharacters.tsx
@@ -3,29 +3,27 @@ import { useState } from "react";
 
 export default function SwCharacters() {
   return (
-    <div>
-      {TabsController()}
-    </div>
+      <TabsController />
   );
 };
 
-function TabsController() {
- const [key, setKey] = useState('home');
+const TabsController = () => {
+  const [key, setKey] = useState('luke');
 
- return (
-  <Tabs id='tab-controller'
-  activeKey={key}
-  onSelect={(k)=>setKey(k as string)}
-  className='mb-3'>
-    <Tab eventKey="luke" title="Luke">
-      
-    </Tab>
-    <Tab eventKey="leia" title="Leia">
-      
-    </Tab>
-    <Tab eventKey="Han" title="Han">
-      
-    </Tab>
-  </Tabs>
- )
+  return (
+    <Tabs id='tab-controller'
+      activeKey={key}
+      onSelect={(k) => setKey(k as string)}
+      className='mb-3'>
+      <Tab eventKey="luke" title="Luke">
+
+      </Tab>
+      <Tab eventKey="leia" title="Leia">
+
+      </Tab>
+      <Tab eventKey="han" title="Han">
+
+      </Tab>
+    </Tabs>
+  )
 }
