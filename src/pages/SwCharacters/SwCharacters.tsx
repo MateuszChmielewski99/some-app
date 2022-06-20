1c1e2dd35bc4f50b0579d50c523125ac5c88e3ec
Mateusz Kałucki <mateusz.kalucki@pl.abb.com>
2022-06-20 11:43:53 +0200

    deleted a pointless wrap

--- src/pages/SwCharacters/SwCharacters.tsx
+++ src/pages/SwCharacters/SwCharacters.tsx
@@ -7,12 +7,6 @@ import CharacterDetails from "../../components/characterDetails/CharacterDetails
 import LoadingSpinner from "../../components/loadingSpinner/LoadingSpinner";
 
 export default function SwCharacters() {
-  return (
-    <TabController />
-  );
-};
-
-const TabController = () => {
   const [key, setKey] = useState('luke');
   const [isLoading, setIsLoading] = useState(false);
   const [character, setCharacter] = useState<Character>();
@@ -29,22 +23,22 @@ const TabController = () => {
 
   return (
     <Fragment>
-          <Tabs id='tab-controller'
-            activeKey={key}
-            onSelect={(k) => setKey(k as string)}
-            className='mb-3'>
-            <Tab eventKey="luke" title="Luke">
-              {isLoading ? <LoadingSpinner /> : <CharacterDetails name={character?.name} gender={character?.gender} birth_year={character?.birth_year} height={character?.height} />}
-            </Tab>
-            <Tab eventKey="leia" title="Leia">
-              {isLoading ? <LoadingSpinner /> : <CharacterDetails name={character?.name} gender={character?.gender} birth_year={character?.birth_year} height={character?.height} />}
-            </Tab>
-            <Tab eventKey="han" title="Han">
-              {isLoading ? <LoadingSpinner /> : <CharacterDetails name={character?.name} gender={character?.gender} birth_year={character?.birth_year} height={character?.height} />}
-            </Tab>
-            <Tab title="Custom">
-            </Tab>
-          </Tabs>
+      <Tabs id='tab-controller'
+        activeKey={key}
+        onSelect={(k) => setKey(k as string)}
+        className='mb-3'>
+        <Tab eventKey="luke" title="Luke">
+          {isLoading ? <LoadingSpinner /> : <CharacterDetails name={character?.name} gender={character?.gender} birth_year={character?.birth_year} height={character?.height} />}
+        </Tab>
+        <Tab eventKey="leia" title="Leia">
+          {isLoading ? <LoadingSpinner /> : <CharacterDetails name={character?.name} gender={character?.gender} birth_year={character?.birth_year} height={character?.height} />}
+        </Tab>
+        <Tab eventKey="han" title="Han">
+          {isLoading ? <LoadingSpinner /> : <CharacterDetails name={character?.name} gender={character?.gender} birth_year={character?.birth_year} height={character?.height} />}
+        </Tab>
+        <Tab title="Custom">
+        </Tab>
+      </Tabs>
     </Fragment>
   );
 };
