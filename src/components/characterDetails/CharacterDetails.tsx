341c2df37e51307a60e6236c52f3f4aec8ac0320
Mateusz Kałucki <mateusz.kalucki@pl.abb.com>
2022-06-20 11:17:35 +0200

    ?? operator

--- src/components/characterDetails/CharacterDetails.tsx
+++ src/components/characterDetails/CharacterDetails.tsx
@@ -10,10 +10,10 @@ interface CharacterDetailsProps {
 export default function CharacterDetails(props:CharacterDetailsProps) {
   return (
     <div className="padding20">
-      <p>Full name: {props.name || "name"}</p>
-      <p>Gender: {props.gender || "gender"}</p>
-      <p>Birth Year: {props.birth_year || "year"}</p>
-      <p>Height: {props.height || "height"} cm</p>
+      <p>Full name: {props.name ?? "name"}</p>
+      <p>Gender: {props.gender ?? "gender"}</p>
+      <p>Birth Year: {props.birth_year ?? "year"}</p>
+      <p>Height: {props.height ?? "height"} cm</p>
     </div>
   )
 }
