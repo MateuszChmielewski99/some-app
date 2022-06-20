1ebbf33f39378a993a075617ff76234391fce2e7
Mateusz Kałucki <mateusz.kalucki@pl.abb.com>
2022-06-20 11:29:41 +0200

    no class

--- src/pages/SwCharacters/SwCharacters.tsx
+++ src/pages/SwCharacters/SwCharacters.tsx
@@ -1,8 +1,8 @@
 import { Tab, Tabs } from "react-bootstrap";
 import { Fragment, useEffect, useState } from "react";
 import Character from "../../models/Character";
-import SWServiceClass from "../../HttpService/SWService";
-import { CharacterDictionary } from "../../data/CharacterDictionary";
+import SWServiceClass from "../../services/SWService";
+import characters from "../../data/CharacterDictionary";
 import CharacterDetails from "../../components/characterDetails/CharacterDetails";
 import LoadingSpinner from "../../components/loadingSpinner/LoadingSpinner";
 
@@ -16,11 +16,10 @@ const TabController = () => {
   const [key, setKey] = useState('luke');
   const [isLoading, setIsLoading] = useState(false);
   const [character, setCharacter] = useState<Character>();
-  const characterDictionary = new CharacterDictionary();
   useEffect(() => {
     const fetchData = async () => {
       setIsLoading(true);
-      const character = await SWServiceClass.fetchCharacter(characterDictionary.characters.get(key)!);
+      const character = await SWServiceClass.fetchCharacter(characters.get(key)!);
       setCharacter(character);
       setIsLoading(false);
     }
@@ -30,22 +29,22 @@ const TabController = () => {
 
   return (
     <Fragment>
-      <Tabs id='tab-controller'
-        activeKey={key}
-        onSelect={(k) => setKey(k as string)}
-        className='mb-3'>
-        <Tab eventKey="luke" title="Luke">
-          {isLoading ? <LoadingSpinner /> : <CharacterDetails name={character?.name!} gender={character?.gender!} birth_year={character?.birth_year!} height={character?.height!} />}
-        </Tab>
-        <Tab eventKey="leia" title="Leia">
-          {isLoading ? <LoadingSpinner /> : <CharacterDetails name={character?.name!} gender={character?.gender!} birth_year={character?.birth_year!} height={character?.height!} />}
-        </Tab>
-        <Tab eventKey="han" title="Han">
-          {isLoading ? <LoadingSpinner /> : <CharacterDetails name={character?.name!} gender={character?.gender!} birth_year={character?.birth_year!} height={character?.height!} />}
-        </Tab>
-        <Tab eventKey="custom" title="Custom">
-        </Tab>
-      </Tabs>
+          <Tabs id='tab-controller'
+            activeKey={key}
+            onSelect={(k) => setKey(k as string)}
+            className='mb-3'>
+            <Tab eventKey="luke" title="Luke">
+              {isLoading ? <LoadingSpinner /> : <CharacterDetails name={character?.name!} gender={character?.gender!} birth_year={character?.birth_year!} height={character?.height!} />}
+            </Tab>
+            <Tab eventKey="leia" title="Leia">
+              {isLoading ? <LoadingSpinner /> : <CharacterDetails name={character?.name!} gender={character?.gender!} birth_year={character?.birth_year!} height={character?.height!} />}
+            </Tab>
+            <Tab eventKey="han" title="Han">
+              {isLoading ? <LoadingSpinner /> : <CharacterDetails name={character?.name!} gender={character?.gender!} birth_year={character?.birth_year!} height={character?.height!} />}
+            </Tab>
+            <Tab title="Custom">
+            </Tab>
+          </Tabs>
     </Fragment>
   );
 };
