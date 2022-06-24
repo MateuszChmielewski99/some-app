907734ef04978582b59c86f30973caa46641b770
Mateusz Kałucki <mateusz.kalucki@pl.abb.com>
2022-06-24 12:35:52 +0200

    readability improvements

--- src/common/services/SharingService.ts
+++ src/common/services/SharingService.ts
@@ -1,16 +1,17 @@
 export function handleShare(title: string, content: string, url: string) {
-  if (navigator.share) {
-    navigator
-      .share({
-        title: title,
-        text: content,
-        url: url,
-      })
-      .then(() => {
-        console.log("Successfully shared");
-      })
-      .catch((error) => {
-        console.error("Something went wrong", error);
-      });
-  }
+  if (!navigator.share)
+    return;
+    
+  navigator
+    .share({
+      title: title,
+      text: content,
+      url: url,
+    })
+    .then(() => {
+      console.log("Successfully shared");
+    })
+    .catch((error) => {
+      console.error("Something went wrong", error);
+    });
 }
